157d843914760d297a3e3f40f8e0b7f7e828ba52
Xanvier Brouwer <xbrouwer@quintor.nl>
2018-01-14 14:24:37 +0100

    Uitgangspositie

--- MyHandsOnApp/app/src/main/java/nl/quintor/myhandsonapp/Main.java
+++ MyHandsOnApp/app/src/main/java/nl/quintor/myhandsonapp/Main.java
@@ -2,11 +2,23 @@ package nl.quintor.myhandsonapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity  {
+    int buttonClicks = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button helloWorldButton = findViewById(R.id.button);
+        helloWorldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Nu al " + buttonClicks++ +" keer geklikt.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
